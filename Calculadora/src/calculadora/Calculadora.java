a438ab41b2cdcc8b6ce0544a81880876f81b9f16
João Manoel <jmaoliveira.cic@uesc.br>
2023-11-18 17:30:21 -0300

    add calculadora e conversor de temperatura

--- Calculadora/src/calculadora/Calculadora.java
+++ Calculadora/src/calculadora/Calculadora.java
@@ -1,5 +1,53 @@
 package calculadora;
 
-public class Calculadora {
+import java.util.Scanner;
 
+public class Calculadora {
+        public static int soma(int a, int b){
+            return a + b;
+        }
+
+        public static int subtracao(int a, int b){
+            return a - b;
+        }
+
+        public static int multiplicacao(int a, int b){
+            return a * b;
+        }
+
+        public static float divisao(int a, int b){
+            return a / b;
+        }
+
+        public static void main(String[] args) {
+            int a, b, opt;
+
+            Scanner Entrada = new Scanner(System.in);
+
+            System.out.println("Digite dois números inteiros: ");
+            a = Entrada.nextInt();
+            b = Entrada.nextInt();
+
+            System.out.println("Escolha uma operação: ");
+            System.out.println("1 - Soma");
+            System.out.println("2 - Subtração");
+            System.out.println("3 - Multiplicação");
+            System.out.println("4 - Divisão");
+
+            opt = Entrada.nextInt();
+
+            if(opt == 1)
+                System.out.println("Resultado: " + soma(a, b));
+            else if(opt == 2)
+                System.out.println("Resultado: " + subtracao(a, b));
+            else if(opt == 3)
+                System.out.println("Resultado: " + multiplicacao(a, b));
+            else if(opt == 4)
+                System.out.println("Resultado: " + divisao(a, b));
+            else
+                System.out.println("Opção inválida!");
+
+            Entrada.close();
+
+        }
 }
--- ConversorTemperatura/src/conversorTemp/ConversorTemp.java
+++ ConversorTemperatura/src/conversorTemp/ConversorTemp.java
@@ -1,5 +1,38 @@
 package conversorTemp;
 
-public class ConversorTemp {
+import java.util.Scanner;
 
+public class ConversorTemp {
+    public static float celsiusToFahrenheit(float celsius){
+        return (celsius * 9/5) + 32;
+    }
+
+    public static float fahrenheitToCelsius(float fahrenheit){
+        return (fahrenheit - 32) * 5/9;
+    }
+
+    public static void main(String[] args) {
+        float temp;
+        int opt;
+
+        Scanner Entrada = new Scanner(System.in);
+
+        System.out.println("Digite uma temperatura: ");
+        temp = Entrada.nextFloat();
+
+        System.out.println("Escolha uma operação: ");
+        System.out.println("1 - Celsius para Fahrenheit");
+        System.out.println("2 - Fahrenheit para Celsius");
+
+        opt = Entrada.nextInt();
+
+        if(opt == 1)
+            System.out.println("Resultado: " + celsiusToFahrenheit(temp));
+        else if(opt == 2)
+            System.out.println("Resultado: " + fahrenheitToCelsius(temp));
+        else
+            System.out.println("Opção inválida!");
+
+        Entrada.close();
+    }
 }
